9a4e8e1f49c65005e6c9abcac828f0f1a5ad861d
Mark <map010@edu.zealand.dk>
2025-04-24 07:12:55 +0200

    changes

--- 2D-Framework-Game/Objects/Creature.cs
+++ 2D-Framework-Game/Objects/Creature.cs
@@ -1,34 +1,82 @@
-﻿namespace _2D_Framework_Game.Objects.Creatures
+﻿using System;
+using System.Collections.Generic;
+using _2D_Framework_Game.Interfaces;
+using System.Diagnostics;
+using _2D_Framework_Game.Objects.Attack;
+using _2D_Framework_Game.Objects.Inventory;
+using _2D_Framework_Game.Strategies;
+
+namespace _2D_Framework_Game.Objects.Creatures
 {
-    public class Creature
+    public abstract class Creature : IObserver
     {
         public string Name { get; set; }
         public int HitPoints { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
 
-        public Creature(string name, int hitPoints, int x, int y)
+        public ItemInventory Inventory { get; set; }
+
+        protected IAttackStrategy AttackStrategy = new DefaultAttackStrategy();
+        private readonly List<IObserver> observers = new();
+
+        protected Creature(string name, int hitPoints, int x, int y, int inventoryCapacity)
         {
             Name = name;
             HitPoints = hitPoints;
             X = x;
             Y = y;
+            Inventory = new ItemInventory(inventoryCapacity);
+
+  
         }
 
-        public void ReceiveHit(int damage)
+
+        public void RegisterObserver(IObserver observer)
         {
-            HitPoints -= damage;
-            if (HitPoints <= 0)
+            observers.Add(observer);
+        }
+
+        public void NotifyObservers(string message)
+        {
+            foreach (var obs in observers)
             {
-                HitPoints = 0;
-                // Handle creature death logic here
+                obs.OnNotified(message);
             }
         }
 
-        public int Hit()
+        public virtual int Hit()
         {
-            // Example damage calculation; modify this logic as needed
-            return 10;  // Default damage for now
+            return AttackStrategy.ExecuteAttack(new List<IAttackComponent>());
+        }
+
+        public virtual void ReceiveHit(int hit)
+        {
+            HitPoints -= hit;
+            Trace.WriteLine($"{Name} received {hit} damage, remaining HP: {HitPoints}");
+            NotifyObservers($"{Name} was hit and lost {hit} HP.");
+
+            if (HitPoints <= 0)
+            {
+                Trace.WriteLine($"{Name} has died.");
+            }
+        }
+
+        public virtual void Loot(WorldObject obj)
+        {
+            if (Inventory.AddItem(obj))
+            {
+                Trace.WriteLine($"{Name} looted {obj.Name}");
+            }
+            else
+            {
+                Trace.WriteLine($"{Name} could not loot {obj.Name} because the inventory is full.");
+            }
+        }
+
+        public virtual void OnNotified(string message)
+        {
+            Trace.WriteLine($"{Name} received notification: {message}");
         }
     }
 }
--- 2D-Framework-Game/Objects/World.cs
+++ 2D-Framework-Game/Objects/World.cs
@@ -2,22 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using _2D_Framework_Game.Objects.Creatures;
+using _2D_Framework_Game.Objects;
 
 namespace _2D_Framework_Game.Objects.World
 {
     public class World
     {
         public List<Creature> Creatures { get; set; }
+        public List<WorldObject> Objects { get; set; } // Add support for WorldObjects (like weapons, shields)
 
         public World()
         {
             Creatures = new List<Creature>();
+            Objects = new List<WorldObject>(); // Initialize the list of WorldObjects
         }
 
         // Add creatures to the world
         public void AddCreature(Creature creature)
         {
-            Creatures.Add(creature);
+            if (creature != null)
+            {
+                Creatures.Add(creature);
+            }
+            else
+            {
+                Console.WriteLine("Error: Cannot add null creature.");
+            }
+        }
+
+        // Add objects to the world (e.g., weapons, shields)
+        public void AddObject(WorldObject obj)
+        {
+            if (obj != null)
+            {
+                Objects.Add(obj);
+            }
+            else
+            {
+                Console.WriteLine("Error: Cannot add null object.");
+            }
         }
 
         // Show weak creatures (HP < 50)
@@ -39,6 +62,16 @@ namespace _2D_Framework_Game.Objects.World
             foreach (var creature in strongCreatures)
             {
                 Console.WriteLine($"{creature.Name} has {creature.HitPoints} HP.");
+            }
+        }
+
+        // Show all objects in the world
+        public void ShowAllObjects()
+        {
+            Console.WriteLine("Objects in the World:");
+            foreach (var obj in Objects)
+            {
+                Console.WriteLine($"{obj.Name} - {obj.Description}");
             }
         }
     }
